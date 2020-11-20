3e62bbe257b4258497bdecfdd5a5c8ea32dd81d5
Krystian Demidiuk <issensensei@gmail.com>
2020-11-20 12:21:00 +0100

    create fab animation

--- app/src/main/java/com/example/travelex/placeCreate/PlaceCreateFragment.kt
+++ app/src/main/java/com/example/travelex/placeCreate/PlaceCreateFragment.kt
@@ -1,17 +1,20 @@
 package com.example.travelex.placeCreate
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import android.view.*
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.example.travelex.R
+import com.example.travelex.ViewAnimation
 import com.example.travelex.database.Place
 import kotlinx.android.synthetic.main.place_create_fragment.*
 
 class PlaceCreateFragment : Fragment() {
 
     private lateinit var placeCreateViewModel: PlaceCreateViewModel
+    private var rotate = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +30,47 @@ class PlaceCreateFragment : Fragment() {
         placeCreateViewModel = ViewModelProvider(this).get(PlaceCreateViewModel::class.java)
 
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        ViewAnimation.initShowOut(lyt_mic)
+        ViewAnimation.initShowOut(lyt_call)
+        back_drop.visibility = View.GONE
+
+        fab_add.setOnClickListener { v -> toggleFabMode(v) }
+
+        back_drop.setOnClickListener { toggleFabMode(fab_add) }
+
+        fab_mic.setOnClickListener {
+            Toast.makeText(
+                requireContext(),
+                "Voice clicked",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+
+        fab_call.setOnClickListener {
+            Toast.makeText(
+                requireContext(),
+                "Call clicked",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
+    private fun toggleFabMode(v: View) {
+        rotate = ViewAnimation.rotateFab(v, !rotate)
+        if (rotate) {
+            ViewAnimation.showIn(lyt_mic)
+            ViewAnimation.showIn(lyt_call)
+            back_drop.visibility = View.VISIBLE
+        } else {
+            ViewAnimation.showOut(lyt_mic)
+            ViewAnimation.showOut(lyt_call)
+            back_drop.visibility = View.GONE
+        }
     }
 
 
